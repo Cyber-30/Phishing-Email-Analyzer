934e9b0104cc73ade777219ab95debb1cb10492e
Cyber-30 <souryadutta234@gmail.com>
2025-12-26 17:18:34 +0530

    Commenting

--- analyzer.py
+++ analyzer.py
@@ -13,26 +13,26 @@ def run_analysis(email_path):
     header_results = analyze_headers(parsed_email)  # 2. Header analysis
 
     
-    body_results = body_detection(parsed_email)
+    body_results = body_detection(parsed_email) # 3. Body keyword detection
 
     
-    ip_score = check_ip_reputation(header_results.get("sender_ip"))
+    ip_score = check_ip_reputation(header_results.get("sender_ip")) # 4. IP reputation check    
 
     
     if ip_score:
         header_results["ip_reputation_score"] = ip_score
-    risk_results = risk_scoring(header_results, body_results)
+    risk_results = risk_scoring(header_results, body_results)   # 5. Risk scoring
 
-   
+    
     report = generate_soc_report(
-        email_subject=parsed_email.get("Subject", "No Subject"),
+        email_subject=parsed_email.get("Subject", "No Subject"), 
         sender=parsed_email.get("From", "Unknown"),
         recipient=parsed_email.get("To", "Unknown"),
         risk_analysis=risk_results
     )
 
     
-    print_soc_report(report)
+    print_soc_report(report) # 6. Report generation
     return report
 
 if __name__ == "__main__":
