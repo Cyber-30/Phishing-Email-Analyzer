af9e49936fa71e084b96c0e236825732ca87e25c
Cyber-30 <souryadutta234@gmail.com>
2025-12-26 17:17:28 +0530

    Commenting

--- analyzer.py
+++ analyzer.py
@@ -6,25 +6,24 @@ from scoring.risk_scoring import risk_scoring
 from report.final_report import generate_soc_report, print_soc_report
 
 def run_analysis(email_path):
-    # 1. Parse email
-    parsed_email = parse_email(email_path)
+    
+    parsed_email = parse_email(email_path)  # 1. Email parsing
 
-    # 2. Header analysis
-    header_results = analyze_headers(parsed_email)
+    
+    header_results = analyze_headers(parsed_email)  # 2. Header analysis
 
-    # 3. Body keyword analysis
+    
     body_results = body_detection(parsed_email)
 
-    # 4. IP reputation check
+    
     ip_score = check_ip_reputation(header_results.get("sender_ip"))
 
-    # 5. Risk scoring
-    # Merge IP score into header_results for scoring
+    
     if ip_score:
         header_results["ip_reputation_score"] = ip_score
     risk_results = risk_scoring(header_results, body_results)
 
-    # 6. Generate SOC report
+   
     report = generate_soc_report(
         email_subject=parsed_email.get("Subject", "No Subject"),
         sender=parsed_email.get("From", "Unknown"),
@@ -32,10 +31,10 @@ def run_analysis(email_path):
         risk_analysis=risk_results
     )
 
-    # 7. Print SOC report
+    
     print_soc_report(report)
     return report
 
 if __name__ == "__main__":
-    sample_email = "samples/phishing.eml"  # change to legit.eml to test
+    sample_email = "samples/phishing.eml"  
     run_analysis(sample_email)
